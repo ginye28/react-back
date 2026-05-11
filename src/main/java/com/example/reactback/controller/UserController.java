e448e02277fd694ef158b9effc61fe4d5be5fef5
ginye28 <tsna1268@gmail.com>
2026-05-12 02:28:02 +0900

    수정

--- src/main/java/com/example/reactback/controller/UserController.java
+++ src/main/java/com/example/reactback/controller/UserController.java
@@ -22,15 +22,14 @@ public class UserController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF8");
         resp.setCharacterEncoding("UTF8");
-        String username = req.getParameter("username");
 
+        String username = req.getParameter("username");
         ObjectMapper objectMapper = new ObjectMapper();
-        if(users.stream().filter(user -> user.getUsername().equals(username)).count() < 1) {
-            resp.setStatus(404);
-            resp.getWriter().println(objectMapper.writeValueAsString(Map.of("message", "조회된 사용자가 없습니다.")));
+        if (users.stream().filter(user -> user.getUsername().equals(username)).count() < 1) {
+            resp.getWriter().println(objectMapper.writeValueAsString(Map.of("message","조회된 사용자가 없습니다.")));
         } else {
             resp.setStatus(200);
-            resp.getWriter().println(objectMapper.writeValueAsString(Map.of("message", "해당 username의 사용자를 찾았습니다.")));
+            resp.getWriter().println(objectMapper.writeValueAsString(Map.of("message","해당 username의 사용자를 찾았습니다.")));
         }
     }
 
@@ -42,9 +41,8 @@ public class UserController extends HttpServlet {
         String line = "";
 
         StringBuilder stringBuilder = new StringBuilder();
-        while((line = br.readLine()) != null) {
+        while ((line = br. readLine()) != null) {
             stringBuilder.append(line);
-
         }
         ObjectMapper objectMapper = new ObjectMapper();
         UserDto userDto = objectMapper.readValue(stringBuilder.toString(), UserDto.class);
--- src/main/java/com/example/reactback/filter/CorsFilter.java
+++ src/main/java/com/example/reactback/filter/CorsFilter.java
@@ -10,6 +10,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+
         // 모든 도메인 허용 (보안상 특정 도메인만 지정하는 것이 좋습니다)
         response.setHeader("Access-Control-Allow-Origin", "*");
         // 허용할 메서드 (GET, POST, PUT, DELETE 등)
@@ -18,6 +19,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         // 허용할 헤더
         response.setHeader("Access-Control-Allow-Headers", "*");
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
